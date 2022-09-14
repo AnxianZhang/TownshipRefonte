83aa0b8c623a4e64e6f156c78e7eb69e707a7dcc
AnxianZhang <gadanxianzhang@gmail.com>
2022-09-14 22:34:01 +0200

    need to added another class on .frame::bofore an ::after

--- js/accueil.js
+++ js/accueil.js
@@ -1,11 +1,22 @@
+const hasNextAnimation = _ =>{
+    if (_.classList.contains("nextAnim"))
+        _.style.border = "transparent";
+    setTimeout(() =>{
+        if (_.classList.contains("nextAnim") )
+            _.style.border = "2px red solid";
+    },250);
+}
+
 const addNextClass = () => {
     const images = Array.from(document.querySelectorAll(".frame"));
     images.forEach((img) =>{
         img.addEventListener("mouseover", () =>{
             img.classList.add("nextAnim");
+            hasNextAnimation(img);
         });
         img.addEventListener("mouseleave", () =>{
-            img.classList.remove("nextAnim");
+            img.classList.remove("nextAnim"); //test
+            hasNextAnimation(img)
         });
     });
 }
