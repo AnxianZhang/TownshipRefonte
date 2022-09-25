c1c9cc7acc7bafc4f5169889a1f6d6916e42102d
AnxianZhang <gadanxianzhang@gmail.com>
2022-09-25 09:26:46 +0200

    the animation polygons to cities name done

--- js/communes.js
+++ js/communes.js
@@ -1,4 +1,5 @@
 const countriesKey = new Map();
+const polygonsKey = new Map();
 let countries;
 let polygons;
 
@@ -6,6 +7,7 @@ const setCountriesKey = () =>{
     let idx = 0;
     countries.forEach(countrie => {
         countriesKey.set(countrie.textContent, idx);
+        polygonsKey.set(polygons[idx], idx); 
         ++idx;
     });
 }
@@ -18,6 +20,16 @@ const addEvent = () =>{
         countrie.addEventListener("mouseleave", () => {
           polygons[countriesKey.get(countrie.textContent)].classList.remove("change-map-color");
         });
+    });
+
+    let countrieLinks = Array.from(document.querySelectorAll(".ville-name a"));
+    polygons.forEach(polygon => {
+        polygon.addEventListener("mouseover", () => {
+            countrieLinks[polygonsKey.get(polygon)].style.color = "rgb(194,134,57)";
+        });
+        polygon.addEventListener("mouseleave", () => {
+            countrieLinks[polygonsKey.get(polygon)].style.color = "#333";
+        })
     });
 }
 
