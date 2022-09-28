548e915690703d3ecf2ed9b182e26b697e0d3e21
AnxianZhang <gadanxianzhang@gmail.com>
2022-09-28 21:17:06 +0200

    yes

--- js/commun.js
+++ js/commun.js
@@ -1,6 +1,8 @@
 $(window).ready(() =>{
+    // $(window).width() >= 1150
     const menuButton = $("#menu-box");
     $("#button-menu").on("click", () =>{
+        alert($(window).width())
         if (menuButton.hasClass("buttonTriggered"))
             menuButton.removeClass("buttonTriggered");
         else
