dc4f9c622fe3940c563765a1d4abb62a177169b0
AnxianZhang <gadanxianzhang@gmail.com>
2022-10-12 21:26:39 +0200

    the lacking menu was added

--- js/navBar.js
+++ js/navBar.js
@@ -1,15 +1,47 @@
-$(window).ready(() => {
-    const menuButton = $("#menu-box");
-    $("#button-menu").on("click", () => {
-        if (menuButton.hasClass("buttonTriggered"))
-            menuButton.removeClass("buttonTriggered");
+let myMenuButton;
+let blobs = [];
+
+const addEventToMenu = () => {
+    const menuBox = $("#menu-box");
+    menuBox.on("click", () => {
+        if (menuBox.hasClass("buttonTriggered"))
+            menuBox.removeClass("buttonTriggered");
         else
-            menuButton.addClass("buttonTriggered");
+            menuBox.addClass("buttonTriggered");
     });
-});
-  
+}
+
+const startEvent = () => {
+    myMenuButton = $("#button-menu");
+    blobs[0] = $("#blob-1");
+    blobs[1] = $("#blob-2");
+    blobs[2] = $("#blob-3");
+    blobs[3] = $("#blob-4");
+
+    addEventToMenu();
+}
+
+const myInit = () => {
+    startEvent();
+}
+
 $(window).on("resize", () => {
-    if ($(window).width() > 1150 && $("#menu-box .blob").eq(0).css("visibility") == "visible")
-        $("#button-menu").click();
+    const enableBlobTransition = () => {
+        for (let i = 0; i < blobs.length; ++i)
+            blobs[i].css("transition", "all .2s ." + (i + 1) + "s ease-in-out");
+    }
+
+    const disablBlobTransistion = () => {
+        for (let i = 0; i < blobs.length; ++i)
+            blobs[i].css("transition", "none");
+    }
+
+    if ($(window).width() >= 1150 && $("#menu-box .blob").eq(0).css("visibility") == "visible") {
+        disablBlobTransistion();
+        myMenuButton.click();
+    }
+    else
+        enableBlobTransition();
 });
-  
+
+$(window).ready(myInit);
