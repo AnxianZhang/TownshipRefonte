99ef3cf22810dc5afed60b023da661c6626504a5
nate11010 <97159977+nate11010@users.noreply.github.com>
2023-02-06 17:23:53 +0100

    drag and drop features
    bouton effacer tout, correction de bug, hover

--- js/draggable.js
+++ js/draggable.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
     $(".draggable").draggable({ cursor: "move", containment: "#choixright", scroll: true, revert: true });
 
+
     $("#resultat").droppable({
+        classes: { "ui-droppable-hover": "ui-state-hover" },
         drop: function (event, ui) {
             let dropped = ui.draggable;
             let droppedOn = $(this);
             $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn).addClass("dropped-item fas fa-times");
+            $("#resultat > div").draggable("disable");
+            $("#resultat > div").removeClass("draggable ui-draggable ui-draggable-handle");
+
         }
     });
+
+    $("#effacer").on("click", function () {
+        $("#resultat > div").draggable("enable");
+        $("#resultat > div").appendTo("#choix").removeClass("dropped-item fas fa-times").addClass("draggable ui-draggable ui-draggable-handle");
+
+    });
+
+    $(".draggable").on("click", function () {
+        alert("caca");
+        /*$(this).draggable("enable");
+        $(this).appendTo("#choix").removeClass("dropped-item fas fa-times").addClass("draggable ui-draggable ui-draggable-handle");*/
+    });
 });
-
-
-
