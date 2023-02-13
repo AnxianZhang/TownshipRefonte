1c7e290d5d5a9792e31cc3492e195571aab16bbb
nate11010 <97159977+nate11010@users.noreply.github.com>
2023-02-13 16:06:19 +0100

    v3 hover + trash

--- js/draggable.js
+++ js/draggable.js
@@ -7,22 +7,21 @@ $(document).ready(function () {
         drop: function (event, ui) {
             let dropped = ui.draggable;
             let droppedOn = $(this);
-            $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn).addClass("dropped-item fas fa-times");
-            $("#resultat > div").draggable("disable");
+            $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn).addClass("dropped-item fas");
             $("#resultat > div").removeClass("draggable ui-draggable ui-draggable-handle");
-
         }
     });
 
     $("#effacer").on("click", function () {
-        $("#resultat > div").draggable("enable");
         $("#resultat > div").appendTo("#choix").removeClass("dropped-item fas fa-times").addClass("draggable ui-draggable ui-draggable-handle");
-
     });
 
-    $(".draggable").on("click", function () {
-        alert("caca");
-        /*$(this).draggable("enable");
-        $(this).appendTo("#choix").removeClass("dropped-item fas fa-times").addClass("draggable ui-draggable ui-draggable-handle");*/
+    $("#poubelle").droppable({
+        classes: { "ui-droppable-hover": "ui-state-hover2" },
+        drop: function (event, ui) {
+            let dropped = ui.draggable;
+            let droppedOn = $("#choix");
+            $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn).removeClass("dropped-item fas fa-times").addClass("draggable ui-draggable ui-draggable-handle");
+        }
     });
 });
