d33fae6856feb71bc312f1df8282fcdfde51d9bf
redou <redouaneo.ro@gmail.com>
2023-03-03 02:00:35 +0100

    ajout d'un systeme de séquence en sql avec une fonction associé

---
+++ sql/fonctionGetID.sql
@@ -0,0 +1,12 @@
+DELIMITER //
+
+CREATE FUNCTION getID() RETURNS int DETERMINISTIC
+BEGIN
+
+insert into userseq(num) values(default) ;
+RETURN (select max(num) from UserSeq);
+END
+
+//
+
+DELIMITER ;
