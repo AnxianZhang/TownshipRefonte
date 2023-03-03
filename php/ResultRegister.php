215d14195ae09b74b360802bf533a22d7eba6efb
redou <redouaneo.ro@gmail.com>
2023-03-03 02:02:13 +0100

    enregistrement de l'utilisateur

--- php/ResultRegister.php
+++ php/ResultRegister.php
@@ -1,25 +1,30 @@
 <?php
 $tableau = isset($_POST['tab']) ? $_POST['tab'] : "pas bon";
 
+$firstName = isset($_POST['nom']) ? $_POST['nom'] : "";
+$lastName = isset($_POST['prenom']) ? $_POST['prenom'] : "";
+$age = isset($_POST['age']) ? $_POST['age'] : "";
+
 //echo $tableau[0] . "";
 
 require("connexionBD.php");
 
-$sql = "INSERT INTO table2 (column1, column2, column3, ...)
-SELECT column1, column2, column3,
-FROM table1
-WHERE condition";
+//$sql = "INSERT INTO table2 (column1, column2, column3, ...)
+//SELECT column1, column2, column3,
+//FROM table1
+//WHERE condition";
 
 
-$sql = "INSERT INTO utilisateur VALUES(:id, :Nom,:Prenom,:Age);";
-$null = null;
+
+$sql = "INSERT INTO utilisateur VALUES(getID(), :Nom,:Prenom,:Age);";
+//$null = null;
 try{
 
     $commande = $pdo->prepare($sql);
-    $commande->bindParam(':id', $null);
-    $commande->bindParam(':Nom', $null);
-    $commande->bindParam(':Prenom', $null);
-    $commande->bindParam(':Age', $null);
+
+    $commande->bindParam(':Nom', $lastName);
+    $commande->bindParam(':Prenom', $firstName);
+    $commande->bindParam(':Age', $age);
 
     $commande->execute();
 
@@ -29,23 +34,7 @@ try{
     die();
 }
 
-$sql = "INSERT INTO utilisateur VALUES(:id, :Nom,:Prenom,:Age);";
-$null = null;
-try{
 
-    $commande = $pdo->prepare($sql);
-    $commande->bindParam(':id', $null);
-    $commande->bindParam(':Nom', $null);
-    $commande->bindParam(':Prenom', $null);
-    $commande->bindParam(':Age', $null);
-
-    $commande->execute();
-
-
-} catch(PDOException $e){
-    echo utf8_encode("Echec de la requete SQL dans ResultRegister.php" . $e->getMessage() . "\n");
-    die();
-}
 
 
 
